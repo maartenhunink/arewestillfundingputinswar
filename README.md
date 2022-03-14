5cb920ad62b3a8c5262370896713ac0711ae37b5
Maarten Hunink <maartenhunink@gmail.com>
2022-03-14 02:10:43 +0100

    start using the API from CREA

--- README.md
+++ README.md
@@ -4,19 +4,9 @@ This is the source code for [https://arewestillfundingputinswar.com/][1]
 
 ## Calculations
 
-You can find the calculation for the total funds in [assets/js/magic.js][2], it is based on an estimate of 950 million euro per day. This was calculated by [Simone Tagliapietra][3] from [Breugel.org][4]. You can find more info [here][5].
+The calculations for the estimates are made by the [Centre for Research on Energy and Clean Air (CREA)][2]. Read more about their methodology [here][3]. They were so kind to create an API which this website connects to. 
 
-I would love to find detailed day to day data for oil and gas import and costs. Something similar to [this chart][6]. If you have access to this, please contact me.
-
-## Help
-
-If you can improve the calculations on this site, please don’t hesitate to make changes to the source code or contact me.
-
-Thank you!
 
 [1]:	https://arewestillfundingputinswar.com
-[2]:	https://github.com/maartenhunink/arewestillfundingputinswar/blob/master/assets/js/magic.js
-[3]:	https://twitter.com/Tagliapietra_S
-[4]:	https://breugel.org
-[5]:	https://twitter.com/Tagliapietra_S/status/1499313698405556225
-[6]:	https://twitter.com/Tagliapietra%5C_S/status/1499343758093131776
+[2]:	https://energyandcleanair.org/
+[3]:	https://crea.shinyapps.io/russia_counter/?tab=methodology
--- assets/js/magic.js
+++ assets/js/magic.js
@@ -1,89 +1,94 @@
-var moment = require('moment'); // require
+(function() {
 
+	var moment = require('moment'); // require
 
-window.onload = function(){
+	window.onload = function(){
 
-	let now = moment.utc();
-	let invasion = moment.utc('2022-02-24T03:00:00');
-	var milisecondsSinceInvasion = now.diff(invasion, 'milliseconds');
+		var total_eur = 0;
+		let now = moment.utc();
+		let invasion = moment.utc('2022-02-24T03:00:00');
 
-	let fundsPerDay = 95000000;
-	var fundsPerMilisecond = fundsPerDay / 86400000; 
-	var fundsSinceInvasion = Math.floor(milisecondsSinceInvasion * fundsPerMilisecond);
+		var tanks = 0;
+		var jets = 0;
 
-	var tanks = 0;
-	var jets = 0;
+		var tanksPrice = 914134;
+		var jetsPrice = 36565360;
 
-	var tanksPrice = 914134;
-	var jetsPrice = 36565360;
+		var tweetBtn = document.getElementById('tweet-this');
 
-	var tanksHtml = '';
-	var jetsHtml = '';
+		document.getElementById('days-ago').innerHTML = invasion.fromNow();
 
+		fetch('proxy.php?url=https://api.energyandcleanair.org/v1/russia_counter')
+		  .then(response => response.json())
+		  .then(data => {
 
-	document.getElementById('days-ago').innerHTML = invasion.fromNow();
+        total_eur = data.total_eur;
 
-	var tweetBtn = document.getElementById('tweet-this');
+        setFunds(total_eur);
 
- 	setInterval(function() {
- 		setFunds();
- 	}, 70);
+			 	setInterval(function() {
+			 		total_eur = total_eur + (data.total_eur_per_sec / 10);
+			 		setFunds(total_eur)
+			 	}, 100);
 
- 	function setFunds(){
+	 			setGraphics(total_eur, tanks, jets);
 
- 		milisecondsSinceInvasion = moment.utc().diff(invasion, 'milliseconds');
- 		fundsSinceInvasion = Math.floor(milisecondsSinceInvasion * fundsPerMilisecond);
+			 	setInterval(function() {
+			 		total_eur = total_eur + (data.total_eur_per_sec * 1);
+			 		setGraphics(total_eur, tanks, jets);
+			 	}, 1000);
 
-		var euros = new Intl.NumberFormat({
+    });
+
+	 	function setFunds(total_eur){
+
+			var euros = new Intl.NumberFormat({
+			    currency: `EUR`,
+			    style: 'currency',
+			}).format(Math.floor(total_eur));
+
+		 	document.getElementById('funds-counter').innerHTML = euros;
+	 	}
+
+	 	function setGraphics(total_eur, oldTanks, oldJets){
+
+	 		tanks = Math.floor(total_eur / tanksPrice);
+	 		jets = Math.floor(total_eur / jetsPrice);
+
+	 		var tanksHtml = '';
+
+	 		if(oldTanks != tanks){
+
+		 		for (let i = 0; i < tanks; i++) {
+		  		tanksHtml += "<div class='tank'></div>";
+				}
+
+			 	document.getElementById('tanks').innerHTML = tanksHtml;
+			 	document.getElementById('tanks-amount').innerHTML = tanks;
+
+			}
+			
+	 		var jetsHtml = '';
+
+	 		if(oldJets != jets){
+
+		 		for (let i = 0; i < jets; i++) {
+		  		jetsHtml += "<div class='jet'></div>";
+				}
+
+			 	document.getElementById('jets').innerHTML = jetsHtml;
+			 	document.getElementById('jets-amount').innerHTML = jets;
+
+	 		}
+
+			var euros = new Intl.NumberFormat({
 		    currency: `EUR`,
 		    style: 'currency',
-		}).format(Math.floor(fundsSinceInvasion));
+			}).format(Math.floor(total_eur));
+		
+			tweetBtn.href = "http://twitter.com/share?text=So far the EU has funded Putin's war with €"+euros+" and counting. @stillfundingwar https://arewestillfundingputinswar.com/";
 
-	 	document.getElementById('funds-counter').innerHTML = euros;
- 	}
+	 	}
+	}
 
- 	setGraphics(tanks, jets);
-
- 	setInterval(function() {
- 		setGraphics(tanks, jets);
- 	}, 100000);
-
-
- 	function setGraphics(oldTanks, oldJets){
- 		milisecondsSinceInvasion = moment.utc().diff(invasion, 'milliseconds');
- 		fundsSinceInvasion = Math.floor(milisecondsSinceInvasion * fundsPerMilisecond);
-
- 		tanks = Math.floor(fundsSinceInvasion / tanksPrice);
- 		jets = Math.floor(fundsSinceInvasion / jetsPrice);
-
- 		if(oldTanks != tanks){
-
-	 		for (let i = 0; i < tanks; i++) {
-	  		tanksHtml += "<div class='tank'></div>";
-			}
-
-		 	document.getElementById('tanks').innerHTML = tanksHtml;
-		 	document.getElementById('tanks-amount').innerHTML = tanks;
-
- 		}
-
- 		if(oldJets != jets){
-
-	 		for (let i = 0; i < jets; i++) {
-	  		jetsHtml += "<div class='jet'></div>";
-			}
-
-		 	document.getElementById('jets').innerHTML = jetsHtml;
-		 	document.getElementById('jets-amount').innerHTML = jets;
-
- 		}
-
-		var euros = new Intl.NumberFormat({
-	    currency: `EUR`,
-	    style: 'currency',
-		}).format(Math.floor(fundsSinceInvasion));
-	
-		tweetBtn.href = "http://twitter.com/share?text=So far the EU has funded Putin's war with €"+euros+" and counting. @stillfundingwar https://arewestillfundingputinswar.com/";
-
- 	}
-}
+})();
